7b8aadb07209453aa4dcf77ab928eba48ba4eb10
Hemantkumar Goswami (c) <contact@hemantgoswami.com>
2020-06-30 23:31:57 +0100

    Updates some dependencies and fixes test

--- test/index.js
+++ test/index.js
@@ -1,15 +1,15 @@
 // This file is a placeholder and needs more tests.
 
-'use strict';
-
 var test = require('tape');
+var Moment = require('moment-timezone');
 
 var ephemeris = require('../');
 
 test('dummy test', function (t) {
     t.plan(3);
+    var dateObj = new Moment.tz('10.08.2015 17:09:01', 'DD.MM.YYYY HH:mm:ss', 'UTC')
 
-    var result = ephemeris.getAllPlanets("10.08.2015 17:09:01", 10.0014, 53.5653, 0);
+    var result = ephemeris.getAllPlanets(dateObj, 10.0014, 53.5653, 0);
 
     t.equal(result.date.gregorianTerrestrial, '10.8.2015 17:9:1');
     t.equal(result.observer.longitueGeodetic,  10.0014);
