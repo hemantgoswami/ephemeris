66a1fab3f57a61fe3cf779de8660cca306147c2d
Erik Stenflo <erik@stenflo.org>
2020-07-22 22:50:33 -0600

    deflection.calc() function refactored to take fewer parameter

--- src/astronomy/moshier/deflection.js
+++ src/astronomy/moshier/deflection.js
@@ -1,19 +1,13 @@
 $ns.deflection = {}
 
-$ns.deflection.calc = function (p, q, e, result) {
-  var C // double
-  var i // int
-
-  C = 1.974e-8 / ($const.SE * (1.0 + $const.qe))
-  for (i = 0; i < 3; i++) {
+$ns.deflection.calc = function (p, q, e) {
+  var C = 1.974e-8 / ($const.SE * (1 + $const.qe))
+  for (var i = 0; i < 3; i++) {
     $const.dp[i] = C * ($const.pq * e[i] / $const.SE - $const.ep * q[i] / $const.SO)
     p[i] += $const.dp[i]
   }
-
-  result = result || {}
-
-  result.sunElongation = Math.acos(-$const.ep) / $const.DTR
-  result.lightDeflection = $util.showcor(p, $const.dp)
-
-  return result
+  return {
+    sunElongation: Math.acos(-$const.ep) / $const.DTR,
+    lightDeflection: $util.showcor(p, $const.dp)
+  }
 }
