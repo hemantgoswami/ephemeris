a4cdd9eb7b8a0e9d2decfb28c839a97e923ac359
Hemantkumar Goswami (c) <contact@hemantgoswami.com>
2025-07-26 19:12:27 +0100

    Update dependencies, add ESLint configuration, and clean up .gitignore and updates the version

--- test/index.js
+++ test/index.js
@@ -1,15 +1,15 @@
 // This file is a placeholder and needs more tests.
 
-var test = require('tape');
+var test = require('tape')
 
-var ephemeris = require('../');
+var ephemeris = require('../')
 
 test('dummy test', function (t) {
-    t.plan(3);
-    var dateObj = new Date('2015-08-10T17:09:01.000+00:00');
-    var result = ephemeris.getAllPlanets(dateObj, 10.0014, 53.5653, 0);
+    t.plan(3)
+    var dateObj = new Date('2015-08-10T17:09:01.000+00:00')
+    var result = ephemeris.getAllPlanets(dateObj, 10.0014, 53.5653, 0)
 
-    t.equal(result.date.gregorianTerrestrial, '10.8.2015 17:9:1');
-    t.equal(result.observer.longitudeGeodetic,  10.0014);
-    t.equal(result.observed.sun.apparentLongitudeDms360,  '137°45\'39"');
-});
+    t.equal(result.date.gregorianTerrestrial, '10.8.2015 17:9:1')
+    t.equal(result.observer.longitudeGeodetic,  10.0014)
+    t.equal(result.observed.sun.apparentLongitudeDms360,  '137°45\'39"')
+})
